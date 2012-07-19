906a2c61abc6a0e119023ffa8cdcddfffa1ad31e
Jesse Hodges <hodges.jesse@gmail.com>
2012-07-18 22:26:09 -0500

    small change

--- skrull-client/src/skrull/client/SkrullClientStarter.java
+++ skrull-client/src/skrull/client/SkrullClientStarter.java
@@ -28,8 +28,7 @@ public class SkrullClientStarter extends RmiStarter {
             System.out.println(hello.say());
             
             ViewInterface view = new HelloClient();
-           // UnicastRemoteObject.unexportObject(hello, true);
-           Remote engineStub = UnicastRemoteObject.exportObject(view, 0);
+            Remote engineStub = UnicastRemoteObject.exportObject(view, 0);
 
             registry.rebind(ControllerInterface.SERVICE_NAME, engineStub);
             

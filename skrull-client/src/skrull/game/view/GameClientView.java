4446ed5cc31e891d263320164e7fac762f24f6f7
Jesse Hodges <hodges.jesse@gmail.com>
2012-07-23 20:15:24 -0500

    tentative steps toward default client view

--- skrull-client/src/skrull/game/view/GameClientView.java
+++ skrull-client/src/skrull/game/view/GameClientView.java
@@ -1,12 +1,15 @@
 package skrull.game.view;
 
-import java.awt.BorderLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.UUID;
 
 import javax.swing.*;
 
+import skrull.game.factory.IGameFactory;
+import skrull.game.factory.IGameFactory.GameType;
 import skrull.game.model.IGameModel;
 import skrull.rmi.client.IClientListener;
 
@@ -15,13 +18,13 @@ public class GameClientView extends JFrame {
 	public IClientListener _unnamed_ClientListener_;
 	public ClientInputHandler cih;
 	private UUID playerId;
-	private JTextField textField = new JTextField();
+	private JTextField textField;
 
 	public GameClientView(ClientInputHandler cih, UUID playerId) {
 		this.cih = cih;
 		this.playerId = playerId;
 		
-		drawBoard();
+		initBoard();
 		
 		
 		
@@ -29,18 +32,32 @@ public class GameClientView extends JFrame {
 
 	public void modelChanged(IGameModel model) {
 		System.out.println("model changed.. " + model);
-		updateBoard();
+		updateBoard(model);
 	}
 
-	public void drawBoard() {
-			textField.setText("0");
-	        this.getContentPane().add(textField, BorderLayout.NORTH);
+	private void initBoard() {
+			Handler handler = new Handler(); 
+			textField = new JTextField();
+			textField.setText("0123456789");
+	        getContentPane().add(textField, BorderLayout.NORTH);
+	        JPanel buttonPanel = new JPanel(); 
+	        List<JButton> gameButtons = new ArrayList<JButton>();
+	        for (GameType t: IGameFactory.GameType.values()){
+	        	JButton b = new JButton(t.toString());
+	    		b.addActionListener(handler); 
+	    		buttonPanel.add(b);
+	    		gameButtons.add(b);
+	        }
+	        
+	    	buttonPanel.setLayout(new GridLayout(1, buttonPanel.getComponentCount(), 5, 5));
+	    	getContentPane().add(buttonPanel);
 	        pack();
+	        setVisible(true);
 
 	}
 
-	public void updateBoard() {
-		throw new UnsupportedOperationException("actually really great!");
+	private void updateBoard(IGameModel model) {
+		textField.setText("got a message from the model - player id " + playerId + " " + model.getGameType());
 	}
 	
     // Inner classes for Event Handling 

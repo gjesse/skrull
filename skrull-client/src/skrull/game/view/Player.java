09f2c3453e674b6ff1fde4f881f437f6701e4770
jhodges <hodges.jesse@gmail.com>
2012-07-30 21:55:00 -0500

    override hashCode() and equals() for player

--- skrull-client/src/skrull/game/view/Player.java
+++ skrull-client/src/skrull/game/view/Player.java
@@ -17,5 +17,37 @@ public class Player implements IPlayer {
 	public UUID getPlayerId() {
 		return playerId;
 	}
+	
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((playerId == null) ? 0 : playerId.hashCode());
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Player other = (Player) obj;
+		if (playerId == null) {
+			if (other.playerId != null) {
+				return false;
+			}
+		} else if (!playerId.equals(other.playerId)) {
+			return false;
+		}
+		return true;
+	}
 
 }

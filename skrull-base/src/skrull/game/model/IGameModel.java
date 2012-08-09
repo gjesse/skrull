043f0b228c9c96a60dac65a259364be41b76f9b0
Jesse Hodges <hodges.jesse@gmail.com>
2012-08-09 12:46:41 -0500

    javaDocs for IGameModel

--- skrull-base/src/skrull/game/model/IGameModel.java
+++ skrull-base/src/skrull/game/model/IGameModel.java
@@ -6,66 +6,154 @@ import java.util.Collection;
 import skrull.SkrullGameException;
 import skrull.game.factory.IGameFactory.GameType;
 import skrull.game.view.IClientAction;
+import skrull.game.view.IClientAction.ActionType;
 
+/**
+ * Common interface for all game models. 
+ * 
+ * @author jesse
+ *
+ */
 public interface IGameModel extends Serializable {
 
+	/**
+	 * Process an {@link IClientAction.ActionType} of type CHAT
+	 * 
+	 * @param aAction
+	 */
 	public abstract void chatUpdate(IClientAction aAction);
 
 	/**
-	 * Validation must be done here by the game before
+	 * Process an {@link IClientAction.ActionType} of type JOIN_GAME
+	 * 
+	 * Validation must be done by the implementation before
 	 * adding the new player to the game
+	 * 
+	 * Failure to add the player should throw {@link SkrullGameException}
+	 * 
 	 * @param aAction
 	 * @throws SkrullGameException 
 	 */
 	public abstract void joinGame(IClientAction aAction) throws SkrullGameException;
 
+	/**
+	 * Process an {@link IClientAction.ActionType} of type MOVE
+	 * @param aAction
+	 * @throws SkrullGameException
+	 */
 	public abstract void processMove(IClientAction aAction) throws SkrullGameException;
 
+	/**
+	 * indicate if the game is over
+	 * @return
+	 */
 	public abstract boolean isGameOver();
 
+	/**
+	 * Returns the winning {@link IPlayer} if there is one, or null
+	 * @return IPlayer
+	 */
 	public abstract IPlayer getWinner();
 	
+	/**
+	 * Return the active {@link IPlayer}
+	 * @return IPlayer
+	 */
 	public abstract IPlayer getActivePlayer();
 
 	/**
-	 * 2 types of checks should be performed here: 
-	 * 	1. make sure the active player hasn't exceeded some reasonable threshold (ie walked away from the computer)
-	 *  2. make sure we still have network connectivity to the client
+	 * Perform an activity check for the players in this game
+	 * 
+	 * 	2 types of checks should be performed here: 
+	 * 		1. make sure the active player hasn't exceeded some reasonable threshold (ie walked away from the computer)
+	 *  	2. make sure we still have network connectivity to the client
 	 */
 	public abstract void checkActivity();
 
+	/**
+	 * 
+	 * @return the current {@link GameType}
+	 */
 	public abstract GameType getGameType();
 
+	/**
+	 * @return the current game id
+	 */
 	public abstract int getGameId();
 
+	/**
+	 * Process an {@link IClientAction.ActionType} of type QUIT
+	 * 
+	 * @param action
+	 */
 	public abstract void quit(IClientAction action);
 
+	/**
+	 * @return all {@link IPlayer}s in the game
+	 */
 	public abstract Collection<IPlayer> getPlayers();
 	
+	/**
+	 * @return The contents of the chat buffer
+	 */
 	public abstract String getChatContents();
 
+	/**
+	 * Provide a general status message to all clients that can be displayed
+	 * @return the message
+	 */
 	public abstract String getBroadcastMessage();
 
+	/**
+	 * Each game maintains a reference to all active games. This method allows that reference to 
+	 * be updated
+	 * @param activeGames
+	 */
 	public abstract void setActiveGames(Collection<IGameModel> activeGames);
 	
+	/**
+	 * Each game maintains a reference to all active games. This method gets that reference.
+	 * @param activeGames
+	 */
 	public abstract Collection<IGameModel> getActiveGames();
 
-	// public int getCurrentPlayer();
 
-	// public void setCurrentPlayer(int currentPlayer);
-
+	/**
+	 * 
+	 * @return the current count of moves
+	 */
 	public int getMoveCount();
 
-	public void setMoveCount(int moveCount);
+//	public void setMoveCount(int moveCount);
 
+	/**
+	 * 
+	 * @return the max number of moves that can be made for this game
+	 */
 	public int getMaxMoves();
 
-	public void setMaxMoves(int maxMoves);
+//	public void setMaxMoves(int maxMoves);
 
+	/**
+	 * Process an {@link IClientAction.ActionType} of type CREATE_GAME
+	 * 
+	 * Note that actual game creation is taken care of upstream - this will 
+	 * be called after the game is created. This method is needed to provide a way of 
+	 * passing the value of the model back to the views.
+	 * 
+	 * @param action
+	 */
 	public abstract void createGame(IClientAction action);
 	
+	/**
+	 * @return whether or not the game still needs players to begin
+	 */
 	public abstract boolean needsPlayers();
 
+	/**
+	 * 
+	 * @return IBoard the active board object
+	 */
 	public abstract IBoard getBoard();
 	
 	
